d972020c2d016c3b664174fd7975336e51f1d82d
Den <15vladlenchik1995@gmail.com>
2026-05-24 16:20:03 +0300

    Все задания домашней работы выполнены

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,79 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Задача№1
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ",то он достиг совершеннолетия  ");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ",то он не достиг сове" +
+                    "ршеннолетия ,нужно не много подождать");
         }
+
+        //Задача №2
+        int temperature = 4;
+        if (temperature < 5) {
+            System.out.println("На улице холодно,нужно надеть шапку");
+        } else {
+            System.out.println("На улице тепло,можно идти без шапки");
+        }
+        //Задача №3
+        int speed = 61;
+        if (speed > 60) {
+            System.out.println("Если скорость " + speed + " ,то придётся заплатить штраф ");
+        } else {
+            System.out.println("Если скорость " + speed + ", то можно ездить спокойно");
+        }
+        //Задача №4
+        int ageThreshold = 5;
+        if (ageThreshold >= 2 && ageThreshold <= 6) {
+            System.out.println("Если ребёнку " + ageThreshold + ",то ему нужно ходить в детский сад ");
+        }
+
+        if (ageThreshold >= 7 && ageThreshold <= 17) {
+            System.out.println("Если человеку " + ageThreshold + " ,то ему нужно ходить в школу ");
+        }
+        if (ageThreshold >= 18 && ageThreshold <= 24) {
+            System.out.println("Если возраст человека " + ageThreshold + " ,то его место в университете");
+
+
+        }
+        //Задача №5
+        int attractionAge = 7;
+        boolean correctAge = attractionAge >= 5 && attractionAge <= 14;
+        if (correctAge) {
+            System.out.println("Если возраст ребенка " + attractionAge + ",то можно кататься " + "на аттракционе в сопровождении взрослых ");
+        }
+        if (attractionAge < 5) {
+            System.out.println("Если возраст ребенка " + attractionAge + ",то нельзя ка" + "таться на аттракционе");
+        }
+        if (attractionAge > 14) {
+            System.out.println("Если возраст человека " + attractionAge + ",то можно " +
+                    "кататься без сопровождения взрослых");
+        }
+        //Задача №6
+        int IsThereRoomOrNot = 44;
+        int MaximumCapacity = 102;
+        int CanYouSit = 60;
+        int YouCanStand = MaximumCapacity - CanYouSit;
+        if (IsThereRoomOrNot < MaximumCapacity && IsThereRoomOrNot > CanYouSit) {
+            System.out.println("В вагоне есть места только стоя ");
+        } else if (IsThereRoomOrNot < CanYouSit) {
+            System.out.println("В вагоне есть места сидя");
+        } else {
+            System.out.println("Мест в автобусе нет");
+        }
+        //Задача №7
+        int one = 9;
+        int two = 2;
+        int three = 6;
+        if (one >= two && one >= three) {
+            System.out.println("Наибольшее число:" + one);
+        } else if (two >= one && two >= three) {
+            System.out.println("Наибольшее число:" + two);
+        } else if (three >= one && three >= two) {
+            System.out.println("Наибольшее число:" + three);
+        }
+
+
     }
 }
